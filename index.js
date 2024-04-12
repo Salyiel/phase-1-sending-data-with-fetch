9e69a3baca488d4fcb87d8b68234f88403a4cc73
Stacy <salyiela@icloud.com>
2024-04-12 19:51:26 +0300

    Completed lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+function submitData(name, email){
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            name: `${name}`,
+            email: `${email}`
+        }),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function (object) {
+        document.body.append(object.id)
+    })
+    .catch(function(error){
+        alert("Bad things happening here!");
+        document.body.append(error.message);
+    });
+}
+submitData()
